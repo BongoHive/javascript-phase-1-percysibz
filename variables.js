4d8a2d5f25de9406f2454d993e4bd295df7a2958
percysibz <33868706+percysibz@users.noreply.github.com>
2019-06-05 22:31:26 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,1 +1,8 @@
 // you solution should be here
+var birth_year = 1993;
+var future_year = 2000;
+var this_year = 2019; 
+var first_birthday = this_year - birth_year;
+var second_birthday = this_year - future_year;
+
+
