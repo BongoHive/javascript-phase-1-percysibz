73956ead1d273695c73cabe47dbce3640e9313e9
percysibz <33868706+percysibz@users.noreply.github.com>
2019-06-11 08:55:25 +0200

    completed
    assignment phase 1, working with variables

--- variables.js
+++ variables.js
@@ -1,8 +1,31 @@
 // you solution should be here
+// exercise number 1
+const type="exercise numnber 1"
 var birth_year = 1993;
-var future_year = 2000;
+var future_year = 2030;
 var this_year = 2019; 
 var first_birthday = this_year - birth_year;
-var second_birthday = this_year - future_year;
+var second_birthday = future_year - birth_year;
+
+
+//Exercise number2
+const type2="exercise numnber 2"
+var current_age= 25;
+var maximum_age =80;
+var estimated_num =3;
+var total=(maximum_age-current_age)*estimated_num;
+
+console.log(type);
+console.log('my birth year is ' + birth_year);
+console.log('first future year is ' + future_year);
+console.log('second future year is ' + this_year);
+console.log('first birthday i will be ' + first_birthday + ' years old');
+console.log('second birthday i will be ' + second_birthday + ' years old');
+
+console.log(type2);
+console.log('current age is ' + current_age);
+console.log('predicted maximum age is ' + maximum_age);
+console.log('estimated amount per day ' + estimated_num);
+console.log('You will need '+ total + ' to last you until the ripe old age of ' + maximum_age);
 
 
